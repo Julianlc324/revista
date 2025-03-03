acbebe84829c440e92aaf7bdb7b146b65ba7cea6
Julianlc324 <31931760+Julianlc324@users.noreply.github.com>
2025-03-03 03:17:47 +0000

    Agregar estructura de archivos e index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <!-- Meta -->
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Abusos Redes Sociales </title>
+      <!-- css -->
+      <link rel="stylesheet" href="/proyectos_web/revista/vista/libreria/bootstrap/css/bootstrap.min.css">
+      
+  </head>
+  <body>
+    <h1>Hola mundo </h1>
+      
+  <!-- js -->
+   <script src="/proyectos_web/revista/vista/libreria/bootstrap/js/bootstrap.bundle.min.js"> </script>
+  </body>
+</html>
