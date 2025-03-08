d6bf9fdbcff8e65e4a24ace1f6fe3c0b5ca2502e
Julianlc324 <31931760+Julianlc324@users.noreply.github.com>
2025-03-08 16:29:03 +0000

    Modificar archivo index.php Modificar header, seccion1 y seccion2

--- index.php
+++ index.php
@@ -7,11 +7,67 @@
       <title>Abusos Redes Sociales </title>
       <!-- css -->
       <link rel="stylesheet" href="/proyectos_web/revista/vista/libreria/bootstrap/css/bootstrap.min.css">
+      <link rel="stylesheet" href="/proyectos_web/revista/vista/assets/icons/font-awesome.min.css">
+      <!-- Temp -->
+      <link rel="stylesheet" href="/proyectos_web/revista/vista/pagina/index.css">
       
   </head>
   <body>
-    <h1>Hola mundo </h1>
-      
+    <header>
+      <div class="container">
+        <div class="row">
+          <!-- Seccion1 -->
+          <div class="col-6 col-md-3">
+            <picture class="img-logo">
+               <img src="/proyectos_web/revista/vista/assets/img/logo/logo01.png" class="img-fluid" alt="logo"/>
+            </picture>
+          </div>
+          <!-- Seccion2 -->
+          <div class="col-6 col-md-9">
+            <nav class="navbar navbar-expand-sm">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMenu">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="collapseMenu">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#" aria-current="page"> 
+                      <i class="fa fa-home" aria-hidden="true"> </i> Inicio 
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#" aria-current="page"> 
+                      <i class="fa fa-book" aria-hidden="true"></i> Revistas 
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"> 
+                      <i class="fa fa-user" aria-hidden="true"> </i> Nosotros 
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      <i class="fa fa-envelope" aria-hidden="true"> </i> Contáctenos
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col img-overlay">
+            <img src="/proyectos_web/revista/vista/assets/img/banner/banner01.png" class="img-fluid" alt="...">
+            <div class="text-overlay">
+              <p class="titulo"> Abusos en las redes sociales </p>
+              <p class="subtitulo"> Detrás de la pantalla, el respeto también debe prevalecer </p>
+              <button type="button" class="btn btn-overlay"> Leer más </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  
   <!-- js -->
    <script src="/proyectos_web/revista/vista/libreria/bootstrap/js/bootstrap.bundle.min.js"> </script>
   </body>
